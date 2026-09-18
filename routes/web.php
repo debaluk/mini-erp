ff5b3badbd18edd28aca22f406805924417d59ad
Budiasa <debaluk@gmail.com>
2026-09-18 23:39:39 +0800

    feat: add trial balance excel export route

--- routes/web.php
+++ routes/web.php
@@ -171,6 +171,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/akuntansi/laba-rugi', fn () => app(ModuleController::class)->show('profit-loss'))->middleware('role:owner,akuntansi')->name('akuntansi.laba-rugi');
     Route::get('/akuntansi/laba-rugi/export-excel', [ModuleController::class, 'exportProfitLossExcel'])->middleware('role:owner,akuntansi')->name('akuntansi.laba-rugi.export-excel');
     Route::get('/akuntansi/neraca-saldo', fn () => app(ModuleController::class)->show('trial-balance'))->middleware('role:owner,akuntansi')->name('akuntansi.neraca-saldo');
+    Route::get('/akuntansi/neraca-saldo/export-excel', [ModuleController::class, 'exportTrialBalanceExcel'])->middleware('role:owner,akuntansi')->name('akuntansi.neraca-saldo.export-excel');
     Route::get('/akuntansi/neraca', fn () => app(ModuleController::class)->show('balance-sheet'))->middleware('role:owner,akuntansi')->name('akuntansi.neraca');
     Route::get('/akuntansi/arus-kas', fn () => app(ModuleController::class)->show('cash-flow'))->middleware('role:owner,akuntansi')->name('akuntansi.arus-kas');
     Route::get('/laporan/penjualan', fn () => app(ModuleController::class)->show('sales'))->middleware('role:owner,kasir,akuntansi')->name('laporan.penjualan');
