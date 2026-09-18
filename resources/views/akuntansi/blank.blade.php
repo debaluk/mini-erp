8c5b1bfdcdb01378906e92cc466c315595745446
Budiasa <debaluk@gmail.com>
2026-09-18 19:56:19 +0800

    feat: add blank accounting module view

---
+++ resources/views/akuntansi/blank.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-header fw-semibold">{{ $title }}</div>
+    <div class="card-body">
+        <p class="text-muted mb-0">Modul {{ $title }} belum diimplementasikan.</p>
+    </div>
+</div>
+@endsection
