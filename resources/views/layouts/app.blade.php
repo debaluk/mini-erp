6c99e9e5c3dea7638f126cc2daa36c852138df67
Budiasa <debaluk@gmail.com>
2026-09-18 13:36:34 +0800

    fix: load jquery before datatables bootstrap adapter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,6 +149,7 @@
 </main>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/2.3.3/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.3.3/js/dataTables.bootstrap5.js"></script>
 </body>
