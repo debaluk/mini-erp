7c31679d9a3afad5453addec1bf6908f458c9c71
Budiasa <debaluk@gmail.com>
2026-09-19 19:48:50 +0800

    feat: add item creation routes

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::get('/master/unit-conversions', [UnitConversionController::class, 'index'])->middleware('access:master_data')->name('master.unit-conversions');
+    Route::get('/master/produk/tambah', [ErpController::class, 'itemCreate'])->middleware('role:owner,admin')->name('master.item.create');
+    Route::post('/master/produk/tambah', [ErpController::class, 'itemStore'])->middleware('role:owner,admin')->name('master.item.store');
     Route::post('/master/unit-conversions', [UnitConversionController::class, 'store'])->middleware('access:master_data')->name('master.unit-conversions.store');
     Route::put('/master/unit-conversions/{id}', [UnitConversionController::class, 'update'])->middleware('access:master_data')->name('master.unit-conversions.update');
     Route::delete('/master/unit-conversions/{id}', [UnitConversionController::class, 'destroy'])->middleware('access:master_data')->name('master.unit-conversions.delete');
