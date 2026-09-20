05eb00b8cf7d64c0b36a5242fbbb48deb590459a
Budiasa <debaluk@gmail.com>
2026-09-20 13:01:17 +0800

    fix: import purchase report controller

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\PosController;
 use App\Http\Controllers\ProductionController;
 use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\PurchaseReportController;
 use App\Http\Controllers\SalesReturnController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\SellingPriceController;
