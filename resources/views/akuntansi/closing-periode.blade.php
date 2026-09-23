344b4cfecd2d5cc5b02314cda069c229c6f35770
Budiasa <debaluk@gmail.com>
2026-09-23 21:09:15 +0800

    chore: prepare closing period placeholder page

---
+++ resources/views/akuntansi/closing-periode.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <strong>Closing Periode</strong>
+    </div>
+    <div class="card-body">
+        <div class="alert alert-secondary mb-0">
+            Halaman belum diimplementasikan. Struktur halaman sudah disiapkan.
+        </div>
+    </div>
+</div>
+@endsection
