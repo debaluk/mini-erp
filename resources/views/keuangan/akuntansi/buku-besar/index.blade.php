b43df3d9fc4f0270749a261d06ed3eb03cc49591
Budiasa <debaluk@gmail.com>
2026-09-25 01:48:19 +0800

    refactor: add keuangan blade structure

---
+++ resources/views/keuangan/akuntansi/buku-besar/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<div class="card shadow-sm"><div class="card-header fw-semibold">Buku Besar</div><div class="card-body"><div class="alert alert-info mb-0">Halaman Buku Besar sudah disiapkan. Fungsi laporan akan menggunakan data jurnal yang ada.</div></div></div>
+@endsection
