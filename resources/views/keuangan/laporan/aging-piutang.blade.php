2d2b5eff13af915a383578dbbc3fe56e15f0c53a
Budiasa <debaluk@gmail.com>
2026-09-25 01:48:34 +0800

    refactor: add keuangan blade structure

---
+++ resources/views/keuangan/laporan/aging-piutang.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<div class="card shadow-sm"><div class="card-header fw-semibold">Aging Piutang</div><div class="card-body"><div class="alert alert-info mb-0">Struktur laporan Aging Piutang sudah disiapkan. Engine piutang tidak diubah pada tahap ini.</div></div></div>
+@endsection
