974c07c57141695ada3f300a9099635fea80017c
Budiasa <debaluk@gmail.com>
2026-09-25 01:48:16 +0800

    refactor: add keuangan blade structure

---
+++ resources/views/keuangan/akuntansi/jurnal-umum/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<div class="card shadow-sm"><div class="card-header fw-semibold">Jurnal Umum</div><div class="card-body"><div class="alert alert-info mb-0">Halaman Jurnal Umum sudah disiapkan. Fungsi jurnal tetap menggunakan engine akuntansi yang ada.</div></div></div>
+@endsection
