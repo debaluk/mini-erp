ad73b889cf5891730f379f8be36f828ed37eebb3
Budiasa <debaluk@gmail.com>
2026-09-25 01:48:21 +0800

    refactor: add keuangan blade structure

---
+++ resources/views/keuangan/akuntansi/closing/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<div class="card shadow-sm"><div class="card-header fw-semibold">Closing Akuntansi</div><div class="card-body"><div class="alert alert-info mb-0">Halaman Closing sudah disiapkan. Engine closing belum diubah pada tahap ini.</div></div></div>
+@endsection
