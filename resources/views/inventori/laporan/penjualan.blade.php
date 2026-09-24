c5e44c61abd2fa02f6888f8c64ea60b1754ad56c
Budiasa <debaluk@gmail.com>
2026-09-25 01:41:16 +0800

    refactor: complete inventori blade tree

---
+++ resources/views/inventori/laporan/penjualan.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+<div class="d-flex justify-content-between align-items-center mb-3"><div><h4 class="mb-1">Laporan Penjualan</h4><div class="text-secondary small">Laporan operasional penjualan.</div></div></div>
+<div class="card shadow-sm"><div class="card-body text-center py-5"><h6 class="mb-2">Laporan Penjualan</h6><div class="text-secondary">Halaman sudah disiapkan. Fungsi akan dilanjutkan pada tahap implementasi fungsi.</div></div></div>
+@endsection
